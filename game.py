5bb0c7b95d69a0ce96eb2d0ac47ac92b370957d2
SSDS-sds <warwicklee1@gmail.com>
2025-03-01 18:12:59 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,61 @@
+
+
+group_size = ""
+group_name = ""
+group_date = ""
+group_venue = ""
+group_theme = ""
+
+
+
+group_size = input("What is the size of your group?")
+group_name = input("What is the name of the group?")
+group_date = input("What time is the competition held?")
+group_venue = input("What venue is your group in?")
+group_theme = input("What is your group's theme?")
+
+
+group_info1 = (group_size,group_name,group_date,group_venue,group_theme)
+
+
+group_size = input("What is the size of your group?")
+group_name = input("What is the name of the group?")
+group_date = input("What time is the competition held?")
+group_venue = input("What venue is your group in?")
+group_theme = input("What is your group's theme?")
+
+
+group_info2 = (group_size,group_name,group_date,group_venue,group_theme)
+
+group_size = input("What is the size of your group?")
+group_name = input("What is the name of the group?")
+group_date = input("What time is the competition held?")
+group_venue = input("What venue is your group in?")
+group_theme = input("What is your group's theme?")
+
+
+group_info3 = (group_size,group_name,group_date,group_venue,group_theme)
+
+group_size = input("What is the size of your group?")
+group_name = input("What is the name of the group?")
+group_date = input("What time is the competition held?")
+group_venue = input("What venue is your group in?")
+group_theme = input("What is your group's theme?")
+
+
+group_info4 = (group_size,group_name,group_date,group_venue,group_theme)
+
+group_size = input("What is the size of your group?")
+group_name = input("What is the name of the group?")
+group_date = input("What time is the competition held?")
+group_venue = input("What venue is your group in?")
+group_theme = input("What is your group's theme?")
+
+group_info5 = (group_size,group_name,group_date,group_venue,group_theme)
+
+
+print(group_info1)
+print(group_info2)
+print(group_info3)
+print(group_info4)
+print(group_info5)
